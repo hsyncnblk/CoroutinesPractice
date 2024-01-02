1f0ca570a6cb87aa3f960ca918fffe2f20dfabd9
huseyin cinibulak <huseyincnblk@gmail.com>
2024-01-02 20:00:37 +0300

    CoroutineScope added

--- app/src/main/java/com/hsyncnblk/coroutinespractice/MainActivity.kt
+++ app/src/main/java/com/hsyncnblk/coroutinespractice/MainActivity.kt
@@ -2,10 +2,40 @@ package com.hsyncnblk.coroutinespractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.hsyncnblk.coroutinespractice.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private  var counter: Int =0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnCount.setOnClickListener {
+            binding.tvCount.text = counter++.toString()
+        }
+
+        binding.btnDownload.setOnClickListener {
+
+            CoroutineScope(Dispatchers.IO).launch {
+                //eğer CoroutineScope kullanmasak indirme işlemi sırasında sayac butonuna tıklarsak uygulama çöker
+
+                for (i in 1 .. 100000){
+                    Log.i("TAG", "Downloading $i in ${Thread.currentThread().name}")
+                }
+
+            }
+
+
+        }
+
+
     }
 }
